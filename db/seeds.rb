9e19f88f6e2165c6b6b985e4e64b394497429462
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-18 08:09:35 +0000

    [Add]ジャンルの初期データを設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#ジャンルの初期設定
+Genre.create!(name: 'おでかけ')
+Genre.create!(name: '生活雑貨')
+Genre.create!(name: 'ファッション')
+Genre.create!(name: 'グルメ')
+Genre.create!(name: 'ライフスタイル')
