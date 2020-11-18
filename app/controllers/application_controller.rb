4c7a12928dc72dc2c02888a98a943ae14c76ae0b
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-18 14:41:32 +0000

    [Add]ログイン画面を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :living_alone_month])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    end
+  
 end
