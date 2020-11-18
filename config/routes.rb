82b36222ce67949e197e1efee608cd7ed318e115
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-18 07:45:52 +0000

    [Add]ルーティングの追加(ログイン機能以外)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    get '/' => 'homes#top', as: "root"
+    get '/homes/about' => 'homes#tabout'
+    
+    resources :users, only: [:show, :edit, :update] do
+        resource :relationships, only: [:create, :destroy]
+  	    get 'following' => 'relationships#following', as: 'following'
+    end
+    
+    get '/users/unsubscribe/:id' => 'users#unsubscribe'
+    patch '/users/withdraw' => 'users#withdraw'
+    
+    resources :articles do
+        resources :favorites, only: [:create, :destroy, :index]
+        resources :article_comments, only: [:create, :destroy]
+    end
+    
 end
