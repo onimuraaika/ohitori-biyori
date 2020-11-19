fb0e596e71c417b03df0e4578d9f46e42627bdde
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-19 13:08:19 +0000

    [Add]会員情報編集画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,25 @@ class UsersController < ApplicationController
     end
 
     def edit#会員情報編集画面
+        @user = User.find(params[:id])
     end
 
     def update#会員情報更新
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user.id)
     end
 
     def unsubscribe#退会確認画面
     end
 
     def withdraw#退会処理
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:profile_image, :nickname, :introduction, :living_alone_month)
     end
 
 end
