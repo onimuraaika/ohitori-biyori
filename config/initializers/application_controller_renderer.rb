18b8fd32832d5eca20e08718cf22cb42f9c778b2
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-19 06:56:54 +0000

    [Add]マイページ・ユーザー詳細ページの作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b03a27a52f017d65dfdfd7547c559c11e602d5ac776c5f244dc45055b44a9f133ad58b2e6dbe6196b5a3482d99d3412445f44ff5a8642087be9f6060ca510b9a'
