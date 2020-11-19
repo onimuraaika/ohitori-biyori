881e4a7286df13b415f9f6e564748a1a09c19c02
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-19 15:07:29 +0000

    [Add]ユーザー表示・投稿表示まで作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
-  
+
     def new #新規投稿画面
         @article = Article.new
     end
-  
+
     def create #投稿処理
         @article = Article.new(article_params)
         @article.user_id = current_user.id
@@ -12,28 +12,30 @@ class ArticlesController < ApplicationController
         else
            render "new"
         end
-  
+
     end
-  
+
     def index#投稿一覧画面
     end
-  
-    def show#投稿詳細画面
+
+    def show #投稿詳細画面
+        @user = User.find(params[:id]) # 部分テンプレート用
+        @article = Article.find_by(params[:id])
     end
-  
+
     def edit#投稿編集画面
     end
-  
+
     def update#投稿内容更新
     end
-  
+
     def destroy#投稿削除
     end
-  
+
     private
-  
+
     def article_params
         params.require(:article).permit(:image, :title, :genre_id, :body)
     end
-  
+
 end
