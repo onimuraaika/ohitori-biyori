95d8d0defdf97f408f810e2353a781912fda3d05
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-21 01:23:00 +0000

    [Add]投稿編集画面を作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
 
-    def new #新規投稿画面
+    def new # 新規投稿画面
         @article = Article.new
     end
 
-    def create #投稿処理
+    def create # 投稿処理
         @article = Article.new(article_params)
         @article.user_id = current_user.id
         if @article.save
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def index #投稿一覧画面
+    def index # 投稿一覧画面
         @genres = Genre.all
         # ジャンル検索
         if @genre = Genre.find_by(name: params[:name])
@@ -24,19 +24,36 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def show #投稿詳細画面
+    def show # 投稿詳細画面
         @article = Article.find(params[:id])
         @user = @article.user # 部分テンプレート用
         @comment = ArticleComment.new
     end
 
-    def edit#投稿編集画面
+    def edit # 投稿編集画面
+        @article = Article.find(params[:id])
+        # 他人の場合は一覧表示画面へ返す
+        if @article.user == current_user
+           render "edit"
+        else
+           redirect_to articles_path
+        end
     end
 
-    def update#投稿内容更新
+    def update # 投稿内容更新
+        @article = Article.find(params[:id])
+        @article.user_id = current_user.id
+        if @article.update(article_params)
+           redirect_to article_path(@article)
+        else
+           render "edit"
+        end
     end
 
-    def destroy#投稿削除
+    def destroy # 投稿削除
+        article = Article.find(params[:id])
+        article.destroy
+        redirect_to articles_path
     end
 
     private
