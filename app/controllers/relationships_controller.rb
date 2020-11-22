d3e7cc9ac4cdd2725a5ff98a7c6182d4c4ff1c9a
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-22 06:31:05 +0000

    [Add/Fix]フォロー一覧表示のviewを調整

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
 
     def create # フォローする
         current_user.follow(params[:user_id])
-  		  redirect_to request.referer
+  		redirect_to request.referer
     end
 
     def destroy # フォローをやめる
         current_user.unfollow(params[:user_id])
-  		  redirect_to request.referer
+  		redirect_to request.referer
     end
 
     def following # フォローしているユーザー一覧
