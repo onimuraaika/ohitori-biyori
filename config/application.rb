1b6122cbc0ded745335ddbd2e92def03bb3b9d8e
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-24 03:01:55 +0000

    [Add]ページネーション のviewを追加、日本語化の日本語を追加

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,17 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module OhitoriBiyori
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    class Application < Rails::Application
+        # Initialize configuration defaults for originally generated Rails version.
+        config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-  end
+        # 日本語化
+        config.i18n.default_locale = :ja
+        config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration can go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded after loading
+        # the framework and any gems in your application.
+    end
 end
