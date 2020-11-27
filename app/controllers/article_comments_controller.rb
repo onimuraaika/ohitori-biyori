8f8a2837a94db05a5cacd60bfd6658bed81e8ed3
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-11-27 06:22:19 +0000

    [Add]コメント機能の非同期、create

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,23 +1,20 @@
 class ArticleCommentsController < ApplicationController
 
     def create # コメント投稿
-        article = Article.find(params[:article_id])
+        @article = Article.find(params[:article_id])
         article_comment = ArticleComment.new(article_comment_params)
-        article_comment.article_id = article.id
+        article_comment.article_id = @article.id
         article_comment.user_id = current_user.id
         article_comment.save
-        if article_comment.save
-      		  redirect_to request.referer
-    		else
-    		    render 'articles/show'
-    		end
+        unless article_comment.save
+    		   render 'articles/show'
+    	end
     end
 
     def destroy # コメント削除
-        article = Article.find(params[:article_id])
-      	article_comment = article.article_comments.find(params[:id])
-    	article_comment.destroy
-    	redirect_to request.referer
+        @article = Article.find(params[:article_id])
+        @article_comment = @article.article_comments.find(params[:id])
+    	@article_comment.destroy
     end
 
     private
