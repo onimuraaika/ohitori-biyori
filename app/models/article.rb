e9538e2f8cee4dc4ff5161040ca20f4759fe2400
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-01 03:20:40 +0000

    [Add]エラーメッセージのcssを追加、部分テンプレートの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,6 +11,7 @@ class Article < ApplicationRecord
 
     validates :image, presence: true
     validates :title, presence: true
+    # validates :genre_id, presence: true
     validates :body, presence: true
 
     # user_idがFavoritesテーブル内に存在するか
