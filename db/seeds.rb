138b8a16adbb45e1ef926dcc6be005384f9d3f84
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-02 09:06:09 +0000

    [Add]サイドバーのジャンルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 
 #ジャンルの初期設定
 Genre.create!(name: 'おでかけ')
+Genre.create!(name: 'グルメ')
 Genre.create!(name: '生活雑貨')
 Genre.create!(name: 'ファッション')
-Genre.create!(name: 'グルメ')
 Genre.create!(name: 'ライフスタイル')
+Genre.create!(name: 'お仕事')
+Genre.create!(name: '節約術')
+Genre.create!(name: 'お悩み')
