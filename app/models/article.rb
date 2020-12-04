7f4f6524132b745c4cb7ac4502093f0af0256c70
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-04 05:51:28 +0000

    [Add]投稿一覧表示のサイドバーを修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,7 @@ class Article < ApplicationRecord
     has_many :article_comments, dependent: :destroy
 
     validates :image, presence: true
-    validates :title, presence: true
+    validates :title, presence: true, length: { maximum: 20 }
     validates :body, presence: true
 
     # user_idがFavoritesテーブル内に存在するか
