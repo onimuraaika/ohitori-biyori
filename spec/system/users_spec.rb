a563f6d095cde4fa5645f3d3aad40d4de7900d3a
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-07 14:07:36 +0000

    [Add]新規投稿画面のテストコードを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
 
-  let!(:user) { create(:user) }
+  describe '新規登録機能のテスト' do
+    let!(:user) { create(:user) }
 
-  describe 'ログイン機能の' do
+    before do
+      visit new_user_registration_path
+      fill_in 'user[nickname]', with: user.nickname
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[living_alone_month]', with: user.living_alone_month
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password_confirmation
+      click_button '新規会員登録する'
+    end
+
+    context '新規会員登録に成功する' do
+      it '投稿一覧画面に遷移する' do
+        expect(current_path).to eq articles_path
+      end
+    end
+    context '新規会員登録に失敗する' do
+      let(:nickname) { nil }
+
+      it '遷移されないこと' do
+        expect(current_path).to eq new_user_registration_path
+      end
+    end
+  end
+
+  describe 'ログイン機能のテスト' do
+    let!(:user) { create(:user) }
     let(:nickname) { nil }
     let(:password) { nil }
 
@@ -23,7 +49,7 @@ RSpec.describe "Users", type: :system do
       end
     end
     context 'ログインに失敗する' do
-      it '遷移されていないこと' do
+      it '遷移されないこと' do
         expect(current_path).to eq new_user_session_path
       end
     end
