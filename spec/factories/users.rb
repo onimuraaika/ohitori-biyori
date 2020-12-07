1cf2dcc3eceee5046ac12f920e7bbb580f8e14b0
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-07 07:27:04 +0000

    [Add]テストコードの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
   	id { |n| n }
     nickname { Faker::Lorem.characters(number: 10) }
-    living_alone_month { Faker::Number(digits: 1) }
+    living_alone_month { Faker::Number.number(digits: 1) }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
