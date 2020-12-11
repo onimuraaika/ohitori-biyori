e9a9b34718f34cb9e81445d5fec272cbfee75809
EC2 Default User <ec2-user@ip-172-31-46-19.ec2.internal>
2020-12-11 08:26:09 +0000

    [Add/Fix]会員情報編集のテストコードを追加、お一人暮らし歴のバリデーションを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,44 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let!(:user) { create(:user) }
 
-  subject { user.valid? }
-
   describe '会員情報編集のバリデーションチェックテスト' do
+    subject { user.valid? }
+    
     context "自己紹介文以外全て入力されていれば保存される" do
       it { is_expected.to be_truthy }
     end
-    context "自己紹介文以外に空欄があるので保存されない" do
+    context "ニックネームが空欄なので保存されない" do
       before { user.nickname = nil }
 
       it { is_expected.to be_falsey }
     end
-    context "おひとり暮らし歴がintegerでないので保存されない" do
-      before { user.living_alone_month = '1' }
+    context "ニックネームが2文字以上なので保存される" do
+      before { user.nickname = 'あ'*2 }
+
+      it { is_expected.to be_truthy }
+    end
+    context "ニックネームが2の字未満なので保存されない" do
+      before { user.nickname = 'あ' }
+
+      it { is_expected.to be_falsey }
+    end
+    context "おひとり暮らし歴が空欄なので保存されない" do
+      before { user.living_alone_month = nil }
+
+      it { is_expected.to be_falsey }
+    end
+    context "おひとり暮らし歴がinteger型でないので保存されない" do
+      before { user.living_alone_month = 'あ' }
+
+      it { is_expected.to be_falsey }
+    end
+    context "自己紹介文が100文字以内なので保存される" do
+      before { user.introduction = 'あ' * 100 }
+
+      it { is_expected.to be_truthy }
+    end
+    context "自己紹介文が101文字以上なので保存されない" do
+      before { user.introduction = 'あ' * 101 }
 
       it { is_expected.to be_falsey }
     end
